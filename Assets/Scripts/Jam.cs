2170d545e853030d18c265e1e92d2ba4ebb0f2ee
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-12-15 12:33:53 +0700

    walk done

---
+++ Assets/Scripts/Jam.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jam : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTranform, secondsTransform;
+    public float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
