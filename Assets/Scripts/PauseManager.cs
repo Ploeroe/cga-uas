c4da20b1a42a7c156e0b824fe02e0655fed7bb39
Fernando Khorasani <fernando.khorasani@gmail.com>
2022-12-15 19:40:58 +0700

    fix pause

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -10,7 +10,10 @@ public class PauseManager : MonoBehaviour
 {
     bool isPaused = false;
     public Text textValue;
+    public Text timeText;
     public string textElement;
+    public string s;
+    float pauseTime;
 
     void Update()
     {
@@ -23,11 +26,22 @@ public class PauseManager : MonoBehaviour
         {
             Time.timeScale = 0;
             textValue.text = "Paused";
+            if (pauseTime == 0)
+            {
+                pauseTime = Time.time;
+                string time = System.DateTime.Now.ToString("hh:mm:ss tt");
+                timeText.text = time;
+            }
         }
         else
         {
             Time.timeScale = 1;
             textValue.text = "";
+            if (pauseTime > 0)
+            {
+                pauseTime = Time.time - pauseTime;
+            }
+            timeText.text = System.DateTime.Now.ToString("hh:mm:ss tt");
         }
     }
 }
