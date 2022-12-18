9ca11532160c6943bb8198ffa16cdbe3ecfca9a4
Fernando Khorasani <fernando.khorasani@gmail.com>
2022-12-18 16:25:50 +0700

    UI text, buka pintu mantap

--- Assets/Scripts/PanelUI.cs
+++ Assets/Scripts/PanelUI.cs
@@ -7,6 +7,8 @@ public class PanelUI : MonoBehaviour
     // Start is called before the first frame update
     public GameObject TextPintuUI;
     public GameObject text;
+    bool bukaPanel = false;
+    public Animator pintuAnim;
 
     void Start()
     {
@@ -14,15 +16,28 @@ public class PanelUI : MonoBehaviour
         text.SetActive(false);
     }
 
-    private void OnCollisionEnter(Collider obj){
-        if(obj.gameObject.tag.Equals("Player")) text.SetActive(true);
+    private void OnTriggerEnter(Collider obj){
+        if(obj.gameObject.tag.Equals("Player")) {
+            text.SetActive(true);
+        }
     }
 
-    private void OnCollisionStay(Collider obj){
-        if(obj.gameObject.tag.Equals("Player")) text.SetActive(true);
+    private void OnTriggerStay(Collider obj){
+        if(obj.gameObject.tag.Equals("Player")) {
+            text.SetActive(true);
+            if(Input.GetKeyDown("e")){
+                if(!bukaPanel){
+                    pintuAnim.SetBool("bukaPanel", true);
+                    bukaPanel = true;
+                }else{
+                    pintuAnim.SetBool("bukaPanel", false);
+                    bukaPanel = false;
+                }
+            }
+        }
     }
 
-    private void OnCollisionExit(Collider obj){
+    private void OnTriggerExit(Collider obj){
         if(obj.gameObject.tag.Equals("Player")) text.SetActive(false);
     }
 
--- Assets/Scripts/TombolPanel.cs
+++ Assets/Scripts/TombolPanel.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TombolPanel : MonoBehaviour
 {
-    bool bukaPanel = false;
-    public Animator pintuAnim;
+    // bool bukaPanel = false;
+    // public Animator pintuAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +16,15 @@ public class TombolPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("e")){
-            if(!bukaPanel){
-                pintuAnim.SetBool("bukaPanel", true);
-                bukaPanel = true;
-            }else{
-                pintuAnim.SetBool("bukaPanel", false);
-                bukaPanel = false;
-            }
-        }
+        // if(Input.GetKeyDown("e")){
+        //     if(!bukaPanel){
+        //         pintuAnim.SetBool("bukaPanel", true);
+        //         bukaPanel = true;
+        //     }else{
+        //         pintuAnim.SetBool("bukaPanel", false);
+        //         bukaPanel = false;
+        //     }
+        // }
         
     }
 }
