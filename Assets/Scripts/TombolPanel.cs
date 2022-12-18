065a423c4904a0e54102574ae33f78cc6f75a71a
Fernando Khorasani <fernando.khorasani@gmail.com>
2022-12-18 14:53:35 +0700

    progress animasi pintu panel

---
+++ Assets/Scripts/TombolPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TombolPanel : MonoBehaviour
+{
+    bool bukaPanel = false;
+    public Animator pintuAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bukaPanel = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
