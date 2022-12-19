8b9a39263d28957d62615d4d07b4c1eec1e9b3fd
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-12-20 04:09:59 +0700

    Update button panel responsive feel

--- Assets/Scripts/PanelPopUp.cs
+++ Assets/Scripts/PanelPopUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PanelPopUp : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PanelPopUp : MonoBehaviour
     public Computer computer;
     public bool isOpenDoor = false;
     public bool isOffGenerator = false;
+    public bool flagOpenDoor = false;
 
     public Light light;
 
@@ -17,20 +19,35 @@ public class PanelPopUp : MonoBehaviour
     }
 
     public void openDoor(){
-        isOpenDoor = true;
-        light.color = new Color(0f, 1f, 0f, 1f);
-        panel.SetActive(false);
+        if(flagOpenDoor){
+            isOpenDoor = false;
+            light.color = new Color(1f, 0f, 0f, 1f);
+        }else{
+            isOpenDoor = true;
+            light.color = new Color(0f, 1f, 0f, 1f);
+        }
         computer.isPanelActive = false;
+        flagOpenDoor = !flagOpenDoor;
+        // get gameobject with tag "ButtonTextOpenDoor"
+        GameObject[] buttonOpenDoor = GameObject.FindGameObjectsWithTag("ButtonTextOpenDoor");
+        foreach(GameObject tempButtonOpenDoor in buttonOpenDoor){
+            tempButtonOpenDoor.GetComponent<TextMeshProUGUI>().text = flagOpenDoor? "Close Door" : "Open Door";
+        }
     }
 
     public void offGenerator(){
         isOffGenerator = !isOffGenerator;
+        // get gameobject with tag "ButtonTextTurnOffGenerator"
+        GameObject[] buttonTurnOffGenerator = GameObject.FindGameObjectsWithTag("ButtonTextTurnOffGenerator");
+        foreach(GameObject tempButtonTurnOffGenerator in buttonTurnOffGenerator){
+            tempButtonTurnOffGenerator.GetComponent<TextMeshProUGUI>().text = isOffGenerator? "Turn On Generator" : "Turn Off Generator";
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
